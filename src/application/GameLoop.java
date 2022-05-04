4f3ee8b0ae849c0272ab2ec5f039b8c4e41b5c13
Lore <30599612+Lorenyx@users.noreply.github.com>
2022-05-03 21:48:29 -0500

    Removed hellofx.fxml

--- src/application/GameLoop.java
+++ src/application/GameLoop.java
@@ -1,20 +1,33 @@
 package application;
 
 public class GameLoop {
-    private Player currentTurn;
-    private Player PLAYER, CPU1, CPU2, CPU3;
+    private Player currentTurn; //  change to int
+    private Player[] players;
     private Deck drawPile;
-
-    public Card cardInPlay;
+    private Deck discardPile;
 
     public GameLoop() {
+        // initialize starting deck
         drawPile = new Deck(); 
-        drawPile.fillDeck();
+        drawPile.init();
+        // initialize empty deck
+        discardPile = new Deck(); // after a player moves a card, place it into discard pile
 
-        PLAYER = new Player("PLUS ONE");
-        CPU1 = new AI("CPU1", AI.Difficulty.EASY);
-        CPU2 = new AI("CPU2", AI.Difficulty.EASY);
-        CPU3 = new AI("CPU3", AI.Difficulty.EASY);
+        Player PLAYER = new Player("PLUS ONE");
+        AI CPU1 = new AI("CPU1", AI.Difficulty.EASY);
+        AI CPU2 = new AI("CPU2", AI.Difficulty.EASY);
+        AI CPU3 = new AI("CPU3", AI.Difficulty.EASY);
+
+        players = new Player[]{PLAYER, CPU1, CPU2, CPU3};
+        drawPile.shuffle(); // shuffle deck of cards
+        // Hand out 7 cards to each player
+        for (int i=0; i<7; i++) {
+            for (int j=0; j<4; j++) {
+                Card c = drawPile.drawCard(); // pull card off top of deck
+                players[j].addCard(c); //  add card to players hand
+            }
+        }
+        // pull inital card to start playing
     }
 
     public void handCardsToPlayers() {
